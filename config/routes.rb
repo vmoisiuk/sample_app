ea012c5495b6b2e95feedd8954c279e555985116
vmoisiuk <moisiuk.volodymyr@gmail.com>
2020-06-03 17:31:20 +0300

    Iteration 3: Basic Layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+  get 'users/new', as: 'sign_up'
   get 'static_pages/help', as: 'help'
   get 'static_pages/about', as: 'about'
-
+  get  'static_pages/contact', as: 'contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
